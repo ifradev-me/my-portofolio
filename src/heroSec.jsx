3419b2f74d7106aa7265b0c82232d7f015a414fe
Ifradil syaifa <110619669+ifradev-me@users.noreply.github.com>
2025-06-17 09:24:28 +0700

    fix: perbaiki hero section
    perbaiki besar dari text

--- src/heroSec.jsx
+++ src/heroSec.jsx
@@ -10,19 +10,16 @@ const HeroSection = () => {
                 className="absolute overflow-hidden w-full h-full rotate-55 opacity-60 "
             />
             
-            <h1 className="flex-shrink-0 text-bold text-3xl sm:text-3xl text-left font-header mb-3 sm:mb-4 z-[1]">
+            <h1 className="flex-shrink-0 text-bold text-4xl sm:text-5xl text-left font-header mb-3 sm:mb-4 z-[1]">
                 Aku Ifrad || Front-End Developer & WhatsApp Automation Builder
             </h1>
-            
-            <div className="flex-1 flex items-center justify-center">
-                <img 
+            <img 
                     src={fotoTes} 
                     alt="Hero image" 
                     className="w-full max-w-md sm:max-w-lg md:max-w-xl h-auto object-cover rounded-lg z-[1] relative left-[26%] object-right self-right" 
                 />
-            </div>
             
-            <p className="flex-shrink-0 text-sm sm:text-md text-left font-body mt-4 sm:mt-6 z-[1]">
+            <p className="flex-shrink-0 text-lg sm:text-2xl text-left font-body mt-4 sm:mt-6 z-[1]">
                 Bangun websitemu & sistem otomatisasi WhatsApp untuk penjualan yang cepat, mudah, dan terukur.
             </p>
         </div>
