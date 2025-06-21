d0024e12c41664af70cd0a9aacbd9a0ef80c85bd
Ifradil syaifa <110619669+ifradev-me@users.noreply.github.com>
2025-06-21 15:18:10 +0700

    test: hilangkan active-section testing

--- src/App.jsx
+++ src/App.jsx
@@ -60,9 +60,7 @@ function App() {
   return (
     <>
       <Navbar whereActive={activeSection}/>
-      <div className="fixed top-4 right-4 bg-white px-4 py-2 shadow rounded z-50">
-        Section aktif: {activeSection}
-      </div>
+     
       <main 
         ref={mainRef}
         className='h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth'
